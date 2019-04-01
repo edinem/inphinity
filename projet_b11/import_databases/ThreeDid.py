63e0db7ed494f2017b48b2c4d946585c6407329a
nyg <nyg@users.noreply.github.com>
2019-04-01 14:38:09 +0200

    Some refactoring, and now able to print list of DDI.

--- projet_b11/import_databases/ThreeDid.py
+++ projet_b11/import_databases/ThreeDid.py
@@ -6,43 +6,58 @@ import subprocess
 
 class ThreeDid:
 
+    # URL which gives us the version of the 3did database
     version_url = 'https://interactome3d.irbbarcelona.org/api/getVersion'
-    db_url = 'https://3did.irbbarcelona.org/download/current/3did.sql.gz'
+
+    # do not use directly – URL where we can find the compressed SQL file
+    db_url = ['https://3did.irbbarcelona.org/download/current/', '3did.sql', '.gz']
+
+    archive_url = ''.join(db_url)
+    archive_filename = ''.join(db_url[1:])
+    sql_filename = db_url[1]
 
     def __init__(self):
         self.version = None
 
-    # retrieve database version
-    def get_version(self):
+    # fetch the database version
+    def fetch_version(self):
         self.version = requests.get(ThreeDid.version_url).content
 
-    # download database on disk
-    def get_db(self):
-        # download archive
-        archive_filename = os.path.basename(ThreeDid.db_url)
+    # download compressed SQL file to disk
+    def download_archive(self):
         archive = requests.get(ThreeDid.db_url)
+        open(ThreeDid.archive_filename, 'wb').write(archive.content)
 
-        # write to disk
-        open(archive_filename, 'wb').write(archive.content)
-
-        # extract
-        with gzip.open(archive_filename, 'rb') as f_in:
-            with open('3did.sql', 'wb') as f_out:
+    # extract compressed SQL file
+    def extract_sql(self):
+        with gzip.open(ThreeDid.archive_filename, 'rb') as f_in:
+            with open(ThreeDid.sql_filename, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
     # import database into mysql
-    def import_db(self, db):
+    # db_cursor: opened database connection
+    def import_db(self, db_cursor):
 
         # recreate database
-        db.execute('drop database if exists 3did')
-        db.execute('create database 3did')
+        db_cursor.execute('drop database if exists 3did')
+        db_cursor.execute('create database 3did')
 
         # import downloaded database
-        out = subprocess.check_output(['mysql', '-u', 'root', '--show-warnings', '-e', 'use 3did; source 3did.sql;'])
+        out = subprocess.check_output(['mysql', '-u', 'root', '--show-warnings', '-e', 'use 3did; source ' + ThreeDid.sql_filename + ';'])
         print(out.decode())
 
-    def test_db(self, db):
-        db.execute('use 3did')
-        db.execute('show tables')
-        for row in db.fetchall():
-            print(row)
+    def print_ddi(self, db_cursor):
+
+        db_cursor.execute('use 3did')
+
+        sql = """select substring_index(d1.pfam_id, '.', 1),
+                        substring_index(d2.pfam_id, '.', 1)
+                   from ddi1
+                  inner join domain d1
+                     on ddi1.domain1 = d1.name
+                  inner join domain d2
+                     on ddi1.domain2 = d2.name;"""
+
+        db_cursor.execute(sql)
+        for d1, d2 in db_cursor.fetchall():
+            print('Domaine 1 : {}, domaine 2 : {}'.format(d1, d2))
--- projet_b11/import_databases/main.py
+++ projet_b11/import_databases/main.py
@@ -1,19 +1,23 @@
 import mysql.connector
-from ThreeDid import ThreeDid
+from ThreeDid import *
 
 # init mysql connection
 try:
-    cnx = mysql.connector.connect(user='root', host='localhost')
-    cur = cnx.cursor()
+    db_connection = mysql.connector.connect(user='root', host='localhost')
+    db_cursor = db_connection.cursor()
 except mysql.connector.Error as e:
     print(e)
 
 did = ThreeDid()
-#did.get_version()
-#print(did.version)
-#did.get_db()
-did.import_db(cur)
-#did.test_db(cur)
 
-cnx.close()
-cur.close()
+# did.fetch_version()
+# print(did.version)
+
+# did.download_archive():
+# did.extract_archive():
+# did.import_sql(db_cursor):
+
+did.print_ddi(db_cursor)
+
+db_connection.close()
+db_cursor.close()
