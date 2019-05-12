8d259fd1b394dd216c27e906b85baed777dd17fb
nyg <nyg@users.noreply.github.com>
2019-05-12 18:17:48 +0200

    Added code in main.py.

--- projet_b11/import_databases/main.py
+++ projet_b11/import_databases/main.py
@@ -1,9 +1,21 @@
+from projet_b11.import_databases.Pfam import Pfam
 from projet_b11.import_databases.ThreeDid import ThreeDid
+from projet_b11.import_databases.DomainInteractionUpdater import DomainInteractionUpdater
 
+inphinity = DomainInteractionUpdater()
+
+# 3did
 did = ThreeDid()
-
 did.fetch_version()
 did.download_archive()
 did.extract_archive()
 did.import_sql()
-did.get_interactions()
+did.fetch_interactions()
+
+#inphinity.update_inphinity_database(did.domain_interactions, '3did')
+
+# Pfam
+pfam = Pfam()
+pfam.get_interactions()
+
+#inphinity.update_inphinity_database(pfam.domain_interactions, 'Pfam')
