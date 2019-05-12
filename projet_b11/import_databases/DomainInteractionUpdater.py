5fb11122c8b958e4710562bd8d8e045c536778b3
nyg <nyg@users.noreply.github.com>
2019-05-12 18:09:50 +0200

    DomainInteractionUpdater: left only one public method (update_inphinity_database).

--- projet_b11/import_databases/DomainInteractionUpdater.py
+++ projet_b11/import_databases/DomainInteractionUpdater.py
@@ -14,7 +14,7 @@ https://www.journaldev.com/23232/python-add-to-dictionary
 import datetime
 
 from configuration.configuration_api import ConfigurationAPI
-from projet_b11.import_databases.Domain_Interaction import DomainInteraction
+from projet_b11.import_databases.DomainInteraction import DomainInteraction
 from rest_client.AuthenticationRest import AuthenticationAPI
 from rest_client.DomainInteractionPairRest import DomainInteractionPairAPI
 from rest_client.DomainInteractionSourceRest import DomainInteractionSourceAPI
@@ -54,7 +54,7 @@ class RESTDomainInteraction:
         self.__init_interaction_source_dict()
 
         # sets to add domains and interactions not present in Inphinity
-        self.new_domain = set()
+        self.new_domains = set()
         self.new_interactions = set()
 
     def __init_domain_dict(self):
@@ -91,56 +91,89 @@ class RESTDomainInteraction:
             interaction_id = interaction_source['domain_interaction']
             self.interaction_source_dict[interaction_id] = interaction_source['information_source']
 
-    def find_new_domain(self, interaction_set):
+    def __find_new_domains(self, interaction_set):
         """
-        For all the interactions in the given set, adds to the new_domain dictionary all domains that do not exists in
-        the Inphinity database.
+        Adds to the new_domain set all domains of interaction_set that do not exists in Inphinity.
 
-        :param interaction_set: A set of interactions with potential new domains
-        :type interaction_set: set - required
+        :param interaction_set: A set of DomainInteraction
+        :type interaction_set: set
         """
         for interaction in interaction_set:
-            if interaction.first_dom not in self.domain_dict.values() and interaction.second_dom not in self.new_domain:
-                self.new_domain.add({interaction.first_dom})
-            if interaction.second_dom not in self.domain_dict.values() and interaction.second_dom not in self.new_domain:
-                self.new_domain.add({interaction.second_dom})
+            if self.domain_dict_reverse[interaction.first_dom] is None:
+                self.new_domains.add(interaction.first_dom)
+            if self.domain_dict_reverse[interaction.second_dom] is None:
+                self.new_domains.add(interaction.second_dom)
 
-    def new_domain_to_data(self):
+    def __find_new_interactions(self, interaction_set):
         """
-        Returns the new_domain dictionary as a list.
+        Adds to the new_interactions set all interactions that do not exist in Inphinity.
 
-        :return: The domain list
-        :rtype: list
-        """
-        domain_list = list()
-        for pfam in self.new_domain:
-            domain_list.append({"designation": pfam})
-        return domain_list
-
-    def find_new_interactions(self, interaction_set):
-        """
-        Finds interactions that do not exist in the Inphinity database and adds them to new_interactions.
-
-        :param interaction_set: A set of potentially new interactions
+        :param interaction_set: A set of DomainInteraction
         :type  interaction_set: set
         """
         for interaction in interaction_set:
-            if self.interaction_dict_reverse.get(interaction) is None:
+            if self.interaction_dict_reverse[interaction] is None:
                 self.new_interactions.add(interaction)
 
-    def new_interaction_to_data(self):
+    def __new_domains_to_list(self):
         """
-        Returns the new_interaction dictionary as a list.
+        Returns the new_domain set as a list.
 
-        :return: The interaction list
+        :return: a list of new domains
         :rtype: list
         """
-        interactions_list = list()
-        for interact in self.new_interactions:
-            interactions_list.append({
-                "domain_a": self.domain_dict_reverse.get(interact.first_dom),
-                "domain_b": self.domain_dict_reverse.get(interact.second_dom)})
-        return interactions_list
+        return [{'designation': d} for d in self.new_domains]
+
+    def __new_interactions_to_list(self):
+        """
+        Returns the new_interaction set as a list.
+
+        :return: a list of new interactions with domain ids
+        :rtype: list
+        """
+        return [{
+            "domain_a": self.domain_dict_reverse[i.first_dom],
+            "domain_b": self.domain_dict_reverse[i.second_dom]
+        } for i in self.new_interactions]
+
+    def __insert_new_domains(self):
+        """
+        Inserts the new domains into Inphinity and update the corresponding dictionaries.
+        """
+        for domain in self.__new_domains_to_list():
+            domain_id = DomainAPI().setDomain(domain)
+            self.domain_dict[domain_id] = domain['designation']
+            self.domain_dict_reverse[domain['designation']] = domain_id
+
+    def __insert_new_interactions(self):
+        """
+        Inserts the new interactions into Inphinity and update the corresponding dictionaries.
+        """
+        for interaction in self.__new_interactions_to_list():
+            interaction_id = DomainInteractionPairAPI().setDomainInteractionPair(interaction)
+            ddi = DomainInteraction(interaction.domain_a, interaction.domain_b)
+            self.interaction_dict[interaction_id] = ddi
+            self.interaction_dict_reverse[ddi] = interaction_id
+
+    def __insert_new_interaction_sources(self, interaction_set, source):
+        """
+        Inserts interaction sources for the given interactions which do not exists for the given source.
+
+        :param interaction_set: interactions which we may need to insert an interaction source for
+        :type  interaction_set: set
+
+        :param source: the source of the interaction
+        :type  source: string
+        """
+        for interaction in interaction_set:
+            interaction_id = self.interaction_dict_reverse[interaction]
+            current_source_id = self.interaction_source_dict[interaction_id]
+            if current_source_id is None or current_source_id != self.source_dict[source]:
+                DomainInteractionSourceAPI().setDomainInteractionSource({
+                    'date_creation': datetime.date.today(),
+                    'domain_interaction': interaction_id,
+                    'source': self.source_dict.get(source)
+                })
 
     def update_inphinity_database(self, interaction_set, source):
         """
@@ -148,61 +181,24 @@ class RESTDomainInteraction:
         The function is going to find all new interactions, all new domain from them and insert the Domain, interactions
         ans interaction source.
 
-        :param interaction_set: A set containing all interactions.
-        :type  interaction_set: set - required
+        :param interaction_set: a set of DomainInteraction
+        :type  interaction_set: set
 
-        :param source: A string which is the database name.
-        :type  source: String - required
+        :param source: a string which is the database name
+        :type  source: string
         """
 
         # 1. check which domain from the given interactions do not exists in Inphinity
-        self.find_new_domain(interaction_set)
+        self.__find_new_domains(interaction_set)
 
         # 2. insert those domains into Inphinity
-        self.insert_new_domain()
+        self.__insert_new_domains()
 
         # 3. check which of the given interactions do not exists in Inphinity
-        self.find_new_interactions(interaction_set)
+        self.__find_new_interactions(interaction_set)
 
         # 4. insert them into Inphinity (only the interaction, not the interaction source)
-        self.insert_new_interaction()
+        self.__insert_new_interactions()
 
         # 5. check which of the given interactions do not have a interaction source corresponding to the given source
-        self.insert_new_interaction_source(interaction_set, source)
-
-    def insert_new_domain(self):
-        """
-        Inserts the new domains into the Inphinity database and update domain dictionary.
-        """
-        for domain in self.new_domain_to_data():
-            new_id = DomainAPI().setDomain(domain)
-            self.domain_dict.update({new_id: domain["designation"]})
-            self.domain_dict_reverse.update({domain["designation"]: new_id})
-
-    def insert_new_interaction(self):
-        """
-        Inserts the new interactions into the Inphinity database and update interaction dictionary.
-        """
-        for interaction in self.new_interaction_to_data():
-            interaction_id = DomainInteractionPairAPI().setDomainInteractionPair(interaction)
-            ddi = DomainInteraction(interaction.domain_a, interaction.domain_b)
-            self.interaction_dict.update({interaction_id: ddi})
-            self.interaction_dict_reverse.update({ddi: interaction})
-
-    def insert_new_interaction_source(self, interaction_set, source):
-        """
-        Inserts interaction sources which do not exists.
-
-        :param interaction_set: interactions which we may need to insert an interaction source for
-        :type interaction_set: set
-        :param source: the source of the interaction
-        :type source: string
-        """
-        for interaction in interaction_set:
-            interaction_id = self.interaction_dict_reverse.get(interaction)
-            current_source = self.interaction_source_dict.get(interaction_id)
-            if current_source is None or current_source != source:
-                DomainInteractionSourceAPI().setDomainInteractionSource({
-                    "date_creation": datetime.date.today(),
-                    "domain_interaction": interaction_id,
-                    "source": self.source_dict.get(source)})
+        self.__insert_new_interaction_sources(interaction_set, source)
